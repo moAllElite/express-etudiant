cf629d6a9f6741b4f91e9997dfa4a002256b7f7f
NIANG MOUHAMED <mouhamedn347@gmail.com>
2024-02-06 17:53:55 +0000

    final-commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-let express = require('express');
-const app = express();
+const express = require('express');
+const appli = express();
 const uri =  require("./database/database_config");
 require('dotenv').config() // import env configs
 const studentRoute = require('./routes/etudiant'); //import all routes from etudiant.js
@@ -7,11 +7,11 @@ const { connect_string } = require('./database/database_config');// CONNECTION S
 
 
 
-app.use(express.json());
-app.use(process.env.URL,studentRoute); //default url http:localhost:3000/api/etudiant
+appli.use(express.json());
+appli.use(process.env.URL,studentRoute); //default url http:localhost:3000/api/etudiant
 
 
-app.listen(process.env.PORT,()=>
+appli.listen(process.env.PORT,()=>
     console.log('votre serveur est sur le port  ', process.env.PORT)
 );
 
